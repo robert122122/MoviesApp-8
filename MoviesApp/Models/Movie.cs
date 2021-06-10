b9e10e1839924fa6b1c33497b72797c1d9a97505
Robert Padurariu <robert.padurariu@emanuel.ro>
2021-06-11 00:19:54 +0300

    add files

---
+++ MoviesApp/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class Movie
+    {
+        public Guid Id {get; set;}
+
+        public string Genre { get; set; }
+
+        public string Author { get; set; }
+
+
+        public string ReleaseDate { get; set; }
+
+    }
+}
